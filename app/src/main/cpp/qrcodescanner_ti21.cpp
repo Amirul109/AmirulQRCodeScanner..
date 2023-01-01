51c8957e0b579c76567cdc5ed786bc332d97471b
Amirul109 <irulmuminin91@gmail.com>
2023-01-01 20:36:42 +0700

    rull

---
+++ app/src/main/cpp/qrcodescanner_ti21.cpp
@@ -0,0 +1,17 @@
+// Write C++ code here.
+//
+// Do not forget to dynamically load the C++ library into your application.
+//
+// For instance,
+//
+// In MainActivity.java:
+//    static {
+//       System.loadLibrary("qrcodescanner_ti21");
+//    }
+//
+// Or, in MainActivity.kt:
+//    companion object {
+//      init {
+//         System.loadLibrary("qrcodescanner_ti21")
+//      }
+//    }
